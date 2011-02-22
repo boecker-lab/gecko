894fe9d23988dba340c89e8f28019392b0963f30
Sascha_Winter <Sascha_Winter@c7ffe42d-ddfc-eb13-ebd0-d6ae8874a85d>
2011-02-22 09:36:38 +0000

    Javadoc for BrowserContentEvent and Listener
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/gecko2/trunk/Gecko2@201 c7ffe42d-ddfc-eb13-ebd0-d6ae8874a85d

--- src/gecko2/event/BrowserContentEvent.java
+++ src/gecko2/event/BrowserContentEvent.java
@@ -5,23 +5,45 @@ import jgecko.geneSequence.view.browser.MultipleGenomesBrowser;
 import java.util.EventObject;
 
 /**
+ * A event that indicates changes to a {@link jgecko.geneSequence.view.browser.GenomeBrowser}
  * @author Leon Kuchenbecker <lkuchenb@inf.fu-berlin.de>
  */
 public class BrowserContentEvent extends EventObject{
-    public enum EventType {scrollValueChanged, zoomFactorChanged}
+    /**
+     * The different types of {@link BrowserContentEvent}s
+     */
+    public enum EventType {
+        /** the scroll value of a {@link jgecko.geneSequence.view.browser.GenomeBrowser} changed */
+        scrollValueChanged,
+        /** the zoom factor of a {@link jgecko.geneSequence.view.browser.GenomeBrowser} changed*/
+        zoomFactorChanged
+    }
 
 	private final EventType eventType;
 	private static final long serialVersionUID = 1382632021469547584L;
 
+    /**
+     * Constructs a new {@code BrowserContentEvent} with the specified {@link MultipleGenomesBrowser} source and an event type
+     * @param source the {@link MultipleGenomesBrowser} that fired the event
+     * @param eventType either {@code scrollValueChanged} or {@code zoomFactorChanged}
+     */
 	public BrowserContentEvent(MultipleGenomesBrowser source, EventType eventType) {
 		super(source);
 		this.eventType = eventType;
 	}
 
+    /**
+     * Return the type of the event
+     * @return the event type
+     */
 	public EventType getEventType() {
 		return eventType;
 	}
 
+    /**
+     * Returns the source of the event
+     * @return the source {@link MultipleGenomesBrowser}
+     */
 	@Override
 	public MultipleGenomesBrowser getSource() {
 		return (MultipleGenomesBrowser) super.getSource();
--- src/gecko2/event/BrowserContentListener.java
+++ src/gecko2/event/BrowserContentListener.java
@@ -3,7 +3,8 @@ package jgecko.geneSequence.view.browser.event;
 import java.util.EventListener;
 
 /**
- * @author Sascha Winter (sascha.winter@uni-jena.de)
+ * The listener interface for receiving events when the content of a {@link jgecko.geneSequence.view.browser.GenomeBrowser} changes.
+ * @author Leon Kuchenbecker <lkuchenb@inf.fu-berlin.de>
  */
 public interface BrowserContentListener extends EventListener {
     void browserContentChanged(BrowserContentEvent e);
