b8a97ca644c29b25c47c3837deda35eab56e187f
swinter <Sascha.Winter@Uni-Jena.de>
2014-04-25 16:17:11 +0200

    Minor Gui bugfix

--- src/main/java/gecko2/GeckoInstance.java
+++ src/main/java/gecko2/GeckoInstance.java
@@ -393,19 +393,22 @@ public class GeckoInstance {
 
     public void setGeckoInstanceData(final DataSet data) {
         this.data = data;
-        if (gui != null){
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    gui.updateViewscreen();
-                    gui.updategcSelector();
-                    if (data.equals(DataSet.getEmptyDataSet()))
-                        gui.changeMode(Gui.Mode.NO_SESSION);
-                    else
-                        gui.changeMode(Gui.Mode.SESSION_IDLE);
-                }
-            });
-        }
+        Runnable updateGui = new Runnable() {
+            @Override
+            public void run() {
+                gui.updateViewscreen();
+                gui.updategcSelector();
+                if (data.equals(DataSet.getEmptyDataSet()))
+                    gui.changeMode(Gui.Mode.NO_SESSION);
+                else
+                    gui.changeMode(Gui.Mode.SESSION_IDLE);
+            }
+        };
+
+        if (SwingUtilities.isEventDispatchThread())
+            updateGui.run();
+        else
+            SwingUtilities.invokeLater(updateGui);
 
         dataUpdated();
     }
